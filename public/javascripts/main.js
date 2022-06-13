be988525a16bcf218de53d099b8ecd42578084e7
Alex <frogitts@gmail.com>
2022-06-12 20:38:30 -0400

    Initial commit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,34 +1,4 @@
-const worstList = [
-  "Whoever has seen their parents naked most recently",
-  "Whoever has most recently cheated on their significant other",
-  "The last person to have made out with a relative",
-  "The last person to have seen genetalia on a big screen",
-  "The last person to have been questioned by a police officer",
-  "The last person to have buried a dead thing",
-  "Whoever's gotten black out drunk most recently",
-  "The person with the most pubic hair",
-  "Whoever has been in the most legal trouble",
-  "Whoever has most recently committed a crime",
-  "Whoever pooped most recently",
-  "Whoever had sex most recently",
-  "The last person to have had a sex dream about their boss",
-  "The person who has gone the longest without showering recently",
-  "The last person caught cheating at a game",
-  "The person who most recently peed in a pool",
-  "The person who last got caught farting when they were purposely trying to hide it",
-  "The person who last spent money on an illegal activity",
-  "Person most recently asked by security to leave a location",
-  "Most recently had to ask a doctor an embarrassing question",
-  "Last person who waved at someone who was waving at someone behind them",
-  "The last person who had to wear someone else's clothes",
-  "The person who most recently had to remove an item of clothing for law enforcement",
-  "The person who most recently threw up",
-  "The person who has gone the longest without having sex",
-  "Whoever last ate a hair that wasn't theirs",
-  "The person who most recently had to clean blood out of something",
-];
-
-const bestList = [
+const list = [
   "Whoever can guess closest to the current time without looking at a clock",
   "Whoever next says they’re going first",
   "The person with the most hair",
@@ -44,28 +14,11 @@ const bestList = [
   "The person who most recently pet a dog",
 ];
 
-let list = worstList;
-
 window.addEventListener("load", chooseFirst(), false);
 
 function chooseFirst() {
   let selection = Math.floor(Math.random() * list.length);
   document.querySelector("#gofirst").innerHTML = list[selection];
-}
-
-function toggleWorst() {
-  console.log("toggle");
-  const state = document.querySelector("#switch").innerHTML;
-  if (state === "worst") {
-    document.querySelector("#switch").innerHTML = "best";
-    list = bestList;
-    document.querySelector("body").classList.add("best");
-  } else {
-    document.querySelector("#switch").innerHTML = "worst";
-    list = worstList;
-    document.querySelector("body").classList.remove("best");
-  }
-  chooseFirst();
 }
 
 function toggleSuggestion() {
